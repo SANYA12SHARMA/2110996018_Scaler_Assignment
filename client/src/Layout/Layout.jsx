2bd25ec4641aaa59f484499b3b69e30854fbfa1a
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-03-14 16:20:33 +0530

    Header component and Layout page created

---
+++ client/src/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Routers from '../routes/Routers'
+const Layout = () => {
+  return (
+    <>
+        <Header />
+        <Routers />
+    </>
+  )
+}
+
+export default Layout
