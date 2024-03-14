cde1c92b48366a57e2152318593b7c486fb2df00
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-03-14 15:32:03 +0530

    Added some columns in cab users schema

--- server/models/CabUsersSchema.js
+++ server/models/CabUsersSchema.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 const CabUserSchema = new mongoose.Schema({
-    userEmail:{type : String, required : true, unique : true},
-    userSource:{type : String, required : true},
-    userDestination:{type : String, required : true}
+    userEmail : { type : String, required : true, unique : true},
+    userSource : { type : String, required : true},
+    userDestination : { type : String, required : true},
+    userBookingTime : { type : Date, default:Date.now},
+    cabChosen : { type : String,
+                  required:true,
+                  enum : ['WagonR', 'Ertiga', 'Tiago','Celerio', 'Swift']},
+    totalTime : { type:Number},
 });
 
 export default mongoose.model("CabUsers", CabUserSchema);
