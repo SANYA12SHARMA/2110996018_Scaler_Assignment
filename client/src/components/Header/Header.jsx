f257aab5f5daa635533e9c17aeab60280b286c01
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-03-14 16:41:42 +0530

    Header component modified

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import logo from '../../assets/images/logo.png';
 const Header = () => {
   return (
-    <div>
-      Header
+    <header>
+    <div className="logo">
+        <img src={logo} alt="Cab Logo" />
     </div>
+    <nav>
+      <ul>
+        <li>
+          Users
+        </li>
+        <li>
+          Cab Bookings
+        </li>
+      </ul>
+    </nav>
+  </header>
   )
 }
 
