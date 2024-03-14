d0c6bb14d927c14224f7653b5e48c78efe47a948
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-03-14 16:47:27 +0530

    Installed react router dom and created users and bookings page

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Routers from '../routes/Routers'
+import { BrowserRouter } from 'react-router-dom'
 const Layout = () => {
   return (
     <>
         <Header />
+        <BrowserRouter>
         <Routers />
+        </BrowserRouter>
+        
     </>
   )
 }
--- client/src/routes/Routers.jsx
+++ client/src/routes/Routers.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import Bookings from '../pages/Bookings'
+import Users from '../pages/Users'
+import {Route,Routes} from 'react-router-dom';
 const Routers = () => {
   return (
-    <div>
-      Routers
-    </div>
+    <Routes>
+      <Route path="/users" element={<Users/>}/>
+      <Route path="/bookings" element={<Bookings/>}/>
+    </Routes>
   )
 }
 
